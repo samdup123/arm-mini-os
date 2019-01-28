72b5a46b53fd4a55912f75660097f54a3706aaca
Nolan Holden <nolanholden@users.noreply.github.com>
2019-01-27 19:55:36 -0500

    generalize the run script

--- run.sh
+++ run.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-gcc -O3 functions.s calc.c -o calc && ./calc
+: ${CC:=arm-linux-gnueabihf-gcc}
+readonly BINARY_NAME=calc
+readonly SOURCE_FILES=(\
+  functions.s \
+  calc.c \
+)
+readonly C_OPTS=(
+  -O3 \
+)
 
+set -x
+
+"${CC}" \
+  "${C_OPTS[@]}" \
+  "${SOURCE_FILES[@]}" \
+  -o "${BINARY_NAME}"
+  
+"./${BINARY_NAME}"
