b020f70a4179dcdcf8ef7465c3bfafebc1e08a9a
Nolan Holden <nolanholden@users.noreply.github.com>
2019-02-12 09:00:06 -0500

    add old build script

---
+++ arm-mini-os/build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+: ${CC:=gcc}
+: ${OBJCOPY:=objcopy}
+
+readonly ELF_NAME=kernel.elf
+readonly IMAGE_NAME=kernel.img
+
+readonly SOURCES=(
+  source/*.c
+  source/*.s
+)
+
+readonly COPTS=(
+  -O3
+  -std=c11
+  # -march=armv6
+  -mtune=arm1176jzf-s
+  -mfloat-abi=hard
+  -ffast-math
+  -Wl,--no-undefined # -T,kernel.ld,-n
+  -Wa,-mfpu=vfpv2
+)
+
+set -x
+
+"${CC}" \
+  "${COPTS[@]}" \
+  "${SOURCES[@]}" \
+  -o "${ELF_NAME}"
+
+"${OBJCOPY}" "${ELF_NAME}" -O binary "${IMAGE_NAME}"
+
