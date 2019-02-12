4ff0405d6c44692c691948ca1ea6d2ec24967e0f
Nolan Holden <nolanholden@users.noreply.github.com>
2019-02-12 18:53:12 -0500

    add time call

--- arm-mini-os/source/utils.h
+++ arm-mini-os/source/utils.h
@@ -1,7 +1,17 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <time.h>
+
 #define nullptr 0
+
+#define debug_print(fmt, ...)                                           \
+  do {                                                                  \
+    if (DEBUG)                                                          \
+      fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, \
+              __VA_ARGS__);                                             \
+    time(0);                                                            \
+  } while (0)
 
 // terminates the program with a custom message
 void die(void (*print_fn)(const char *), const char *msg);
