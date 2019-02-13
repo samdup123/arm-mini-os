2488e04174426720e1ade14519bf706673e5a5e9
Nolan Holden <nolanholden@users.noreply.github.com>
2019-02-12 19:09:45 -0500

    add crlf to LOG function

--- arm-mini-os/source/utils.h
+++ arm-mini-os/source/utils.h
@@ -7,12 +7,12 @@
 
 #define DEBUG 1
 
-#define LOG(level, format, ...)                                           \
-  do {                                                                    \
-    if (DEBUG) {                                                          \
-      uart_printf("[" #level "] %s:%d:%s(): " format, __FILE__, __LINE__, \
-                  __func__, __VA_ARGS__);                                 \
-    }                                                                     \
+#define LOG(level, format, ...)                                               \
+  do {                                                                        \
+    if (DEBUG) {                                                              \
+      uart_printf("\r\n[" #level "] %s:%d:%s(): " format, __FILE__, __LINE__, \
+                  __func__, __VA_ARGS__);                                     \
+    }                                                                         \
   } while (0)
 
 // terminates the program with a custom message
