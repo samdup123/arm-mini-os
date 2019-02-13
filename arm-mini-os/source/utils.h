43c3a85c689820bebfcc49cf934d71f72d150715
Nolan Holden <nolanholden@users.noreply.github.com>
2019-02-12 20:14:12 -0500

    slightly lengthen death loop wait

--- arm-mini-os/source/utils.h
+++ arm-mini-os/source/utils.h
@@ -27,29 +27,28 @@ extern log_level_t min_logging_level;
 //
 // Above would output to terminal:
 //  --> [ERROR] your_file.c:34:your_function(): expected 100, got 101
-#define LOG(level, format, ...)                                               \
-  do {                                                                        \
-    if (LOGGING_ENABLED) {                                                    \
-      const log_level_t log_level = LOG_LEVEL_##level;                        \
-      const int is_fatal = (log_level == LOG_LEVEL_FATAL);                    \
-      const int should_print = is_fatal || (log_level >= min_logging_level);  \
-      if (should_print) {                                                     \
-        uart_printf("\r[" #level "] %s:%d:%s(): " format, __FILE__, __LINE__, \
-                    __func__, __VA_ARGS__);                                   \
-      }                                                                       \
-      if (is_fatal) {                                                         \
-        /* print fatal message forever */                                     \
-        while (1) {                                                           \
-          uart_printf("\n\rfatal: 1stopping forever");                        \
-          uart_printf("\n\rfatal: 2stopping forever");                        \
-                                                                              \
-          /* delaying slightly */                                             \
-          int count = 0xFFFFF;                                                \
-          while (--count > 0)                                                 \
-            ;                                                                 \
-        }                                                                     \
-      }                                                                       \
-    }                                                                         \
+#define LOG(level, format, ...)                                              \
+  do {                                                                       \
+    if (LOGGING_ENABLED) {                                                   \
+      const log_level_t log_level = LOG_LEVEL_##level;                       \
+      const int is_fatal = (log_level == LOG_LEVEL_FATAL);                   \
+      const int should_print = is_fatal || (log_level >= min_logging_level); \
+      if (should_print) {                                                    \
+        uart_printf("\n\r[" #level "] %s:%d:%s(): " format, __FILE__,        \
+                    __LINE__, __func__, __VA_ARGS__);                        \
+      }                                                                      \
+      if (is_fatal) {                                                        \
+        /* print fatal message forever */                                    \
+        while (1) {                                                          \
+          uart_printf("\n\rfatal: stopping system and looping forever");     \
+                                                                             \
+          /* delaying slightly */                                            \
+          for (int count = 0xFFFFF; --count > 0;)                            \
+            for (int count2 = 4; --count2 > 0;)                              \
+              ;                                                              \
+        }                                                                    \
+      }                                                                      \
+    }                                                                        \
   } while (0)
 
 #endif  // UTILS_H_
